f879c89d0f92939f9153440173e05bd8a8949338
sangvo <sangvo2004dev@gmail.com>
2024-09-22 22:41:02 +0700

    Thay đổi namespace

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace demoWeb.Controllers
+namespace HoiTroWebsite.Controllers
 {
     public class DefaultController : Controller
     {
