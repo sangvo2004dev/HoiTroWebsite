c66da73bab7e5aaf3a246580dbde7a3641787dfb
sangvo <sangvo2004dev@gmail.com>
2024-09-22 23:23:37 +0700

    Menu done

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HoiTroWebsite.Models;
 
 namespace HoiTroWebsite.Controllers
 {
     public class DefaultController : Controller
     {
+        // Khai báo
+        HoiTroEntities _db = new HoiTroEntities();
+
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult getMenu()
+        {
+            var v = from t in _db.Menus
+                    where t.hide == false
+                    orderby t.order ascending
+                    select t;
+
+            return PartialView(v.ToList());
         }
     }
 }
