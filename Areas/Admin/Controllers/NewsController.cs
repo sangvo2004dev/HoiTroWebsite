a5d12b8fb02aa1bd385da9dafb47c1a32ba74dbc
PhuocPhung <phuocphung5890@gmail.com>
2024-11-02 01:45:46 +0700

    xong backend quản lý tin tức, còn chỉnh html

---
+++ Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common.EntitySql;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoiTroWebsite.Models;
+
+namespace HoiTroWebsite.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        private HoiTroEntities db = new HoiTroEntities();
+
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var news = db.News.Include(n => n.NewsType);
+            return View(news.ToList());
+        }
+
+        // GET: Admin/News/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // GET: Admin/News/Create
+        public ActionResult Create()
+        {
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title");
+            return View();
+        }
+
+        // POST: Admin/News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "id,title,author,meta,hide,order,datebegin,brief_description,detail_description,newsTypeId,imagePath")] News news, HttpPostedFileBase img)
+        {
+            if (ModelState.IsValid)
+            {
+                var path = "";
+                var filename = "";
+                if (ModelState.IsValid)
+                {
+                    if (img != null)
+                    {
+                        //file =  Guid = Guid.newGuid().toString() + img.FileName;
+                        filename = img.FileName;
+                        path = Path.Combine(Server.MapPath("~/Content/images"), filename);
+                        img.SaveAs(path);
+                        news.imagePath = filename; //Lưu ý
+                    }
+                    else
+                    {
+                        news.imagePath = "logo.png";
+                    }
+                    news.datebegin =  Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                    //news.meta = Functions.ConvertTopUpSign(news.title);
+                    db.News.Add(news);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title", news.newsTypeId);
+            return View(news);
+        }
+
+        // GET: Admin/News/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title", news.newsTypeId);
+            return View(news);
+        }
+
+        // POST: Admin/News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit([Bind(Include = "id,title,author,meta,hide,order,datebegin,brief_description,detail_description,newsTypeId,imagePath")] News news, HttpPostedFileBase img)
+        {
+            var path = "";
+            var filename = "";
+            News temp = getById(news.id);
+
+            if (ModelState.IsValid)
+            {
+                if (img != null)
+                {
+                    // filename = Guid.NewGuid().ToString() + img.FileName;
+                    filename = DateTime.Now.ToString("dd-MM-yy-hh-mm-ss-") + img.FileName;
+                    path = Path.Combine(Server.MapPath("~Content/images"), filename);
+                    img.SaveAs(path);
+                    temp.imagePath = filename; // Lưu ý
+                }
+                // tem.datebegin = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                temp.title = news.title;
+                temp.brief_description = news.brief_description;
+                temp.detail_description = news.detail_description;
+                temp.meta = news.meta;
+                temp.hide = news.hide;
+                temp.order = news.order;
+                db.Entry(temp).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title", news.newsTypeId);
+            return View(news);
+        }
+        public News getById(long id)
+        {
+            return db.News.Where(x => x.id == id).FirstOrDefault();
+        }
+
+        // GET: Admin/News/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: Admin/News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            News news = db.News.Find(id);
+            db.News.Remove(news);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/RoomInfoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoiTroWebsite.Models;
+
+namespace HoiTroWebsite.Areas.Admin.Controllers
+{
+    public class RoomInfoesController : Controller
+    {
+        private HoiTroEntities db = new HoiTroEntities();
+
+        // GET: Admin/RoomInfoes
+        public ActionResult Index()
+        {
+            var roomInfoes = db.RoomInfoes.Include(r => r.RoomType).Include(r => r.Account);
+            return View(roomInfoes.ToList());
+        }
+
+        // GET: Admin/RoomInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomInfo roomInfo = db.RoomInfoes.Find(id);
+            if (roomInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomInfo);
+        }
+
+        // GET: Admin/RoomInfoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.roomTypeId = new SelectList(db.RoomTypes, "id", "title");
+            ViewBag.accountId = new SelectList(db.Accounts, "id", "name");
+            return View();
+        }
+
+        // POST: Admin/RoomInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,title,brief_description,detail_description,price,acreage,area,location,tenant,meta,hide,order,datebegin,roomTypeId,accountId")] RoomInfo roomInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RoomInfoes.Add(roomInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.roomTypeId = new SelectList(db.RoomTypes, "id", "title", roomInfo.roomTypeId);
+            ViewBag.accountId = new SelectList(db.Accounts, "id", "name", roomInfo.accountId);
+            return View(roomInfo);
+        }
+
+        // GET: Admin/RoomInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomInfo roomInfo = db.RoomInfoes.Find(id);
+            if (roomInfo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.roomTypeId = new SelectList(db.RoomTypes, "id", "title", roomInfo.roomTypeId);
+            ViewBag.accountId = new SelectList(db.Accounts, "id", "name", roomInfo.accountId);
+            return View(roomInfo);
+        }
+
+        // POST: Admin/RoomInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,title,brief_description,detail_description,price,acreage,area,location,tenant,meta,hide,order,datebegin,roomTypeId,accountId")] RoomInfo roomInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(roomInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.roomTypeId = new SelectList(db.RoomTypes, "id", "title", roomInfo.roomTypeId);
+            ViewBag.accountId = new SelectList(db.Accounts, "id", "name", roomInfo.accountId);
+            return View(roomInfo);
+        }
+
+        // GET: Admin/RoomInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomInfo roomInfo = db.RoomInfoes.Find(id);
+            if (roomInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomInfo);
+        }
+
+        // POST: Admin/RoomInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RoomInfo roomInfo = db.RoomInfoes.Find(id);
+            db.RoomInfoes.Remove(roomInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
