fbc2417f0776a7ab5e2f3200789ec23a93b8b18f
PhuocPhung <phuocphung5890@gmail.com>
2024-11-04 02:26:36 +0700

    quản lí account

---
+++ Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoiTroWebsite.Models;
+
+namespace HoiTroWebsite.Areas.Admin.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private HoiTroEntities db = new HoiTroEntities();
+
+        // GET: Admin/Accounts
+        public ActionResult Index()
+        {
+            return View(db.Accounts.ToList());
+        }
+
+        // GET: Admin/Accounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "id,name,phoneNum,email,zaloNum,FBlink,avtImage,password,meta,hide,order,datebegin")] Account account, HttpPostedFileBase img)
+        {
+            var path = "";
+            var filename = "";
+            if (ModelState.IsValid)
+            {
+                if (img != null)
+                {
+                    filename = img.FileName;
+                    path = Path.Combine(Server.MapPath("~/Content/images"), filename);
+                    img.SaveAs(path);
+                    account.avtImage = filename;
+                }
+                else
+                {
+                    account.avtImage = "~/Content/images/logo.png";
+                }
+                db.Accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(account);
+        }
+
+        public Account getById(long id)
+        {
+            return db.Accounts.Where(x => x.id == id).FirstOrDefault();
+        }
+
+        // GET: Admin/Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,phoneNum,email,zaloNum,FBlink,avtImage,password,meta,hide,order,datebegin")] Account account, HttpPostedFileBase img)
+        {
+            var path = "";
+            var filename = "";
+            Account temp = getById(account.id);
+            if (ModelState.IsValid)
+            {
+                if(img != null)
+                {
+                    filename = DateTime.Now.ToString("dd-MM-yy-hh-mm-ss-") + img.FileName;
+                    path = Path.Combine(Server.MapPath("~/Content/images"), filename);
+                    img.SaveAs(path);
+                    temp.avtImage = filename;
+                }
+                temp.datebegin = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                temp.name = account.name;
+                temp.phoneNum = account.phoneNum;
+                temp.email = account.email;
+                temp.zaloNum = account.zaloNum;
+                temp.FBlink = account.FBlink;
+                temp.password = account.password;
+                temp.meta = account.meta;
+                temp.hide = account.hide;
+                temp.order = account.order;
+                db.Entry(temp).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/RoomImagesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoiTroWebsite.Models;
+
+namespace HoiTroWebsite.Areas.Admin.Controllers
+{
+    public class RoomImagesController : Controller
+    {
+        private HoiTroEntities db = new HoiTroEntities();
+
+        // GET: Admin/RoomImages
+        public ActionResult Index()
+        {
+            var roomImages = db.RoomImages.Include(r => r.RoomInfo);
+            return View(roomImages.ToList());
+        }
+
+        // GET: Admin/RoomImages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomImage roomImage = db.RoomImages.Find(id);
+            if (roomImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomImage);
+        }
+
+        // GET: Admin/RoomImages/Create
+        public ActionResult Create()
+        {
+            ViewBag.reference_id = new SelectList(db.RoomInfoes, "id", "title");
+            return View();
+        }
+
+        // POST: Admin/RoomImages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,reference_id,imagePath,meta,hide,order,datebegin")] RoomImage roomImage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RoomImages.Add(roomImage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.reference_id = new SelectList(db.RoomInfoes, "id", "title", roomImage.reference_id);
+            return View(roomImage);
+        }
+
+        // GET: Admin/RoomImages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomImage roomImage = db.RoomImages.Find(id);
+            if (roomImage == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.reference_id = new SelectList(db.RoomInfoes, "id", "title", roomImage.reference_id);
+            return View(roomImage);
+        }
+
+        // POST: Admin/RoomImages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,reference_id,imagePath,meta,hide,order,datebegin")] RoomImage roomImage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(roomImage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.reference_id = new SelectList(db.RoomInfoes, "id", "title", roomImage.reference_id);
+            return View(roomImage);
+        }
+
+        // GET: Admin/RoomImages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomImage roomImage = db.RoomImages.Find(id);
+            if (roomImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomImage);
+        }
+
+        // POST: Admin/RoomImages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RoomImage roomImage = db.RoomImages.Find(id);
+            db.RoomImages.Remove(roomImage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
