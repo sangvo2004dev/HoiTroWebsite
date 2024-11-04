1dc0a54bbc0f8bccefe2685c2d3b912f8d438a66
sangvo <sangvo2004dev@gmail.com>
2024-11-04 14:08:16 +0700

    Temp

---
+++ Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoiTroWebsite.Models;
+
+namespace HoiTroWebsite.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly HoiTroEntities db = new HoiTroEntities();
+
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var news = db.News.Include(n => n.NewsType);
+            ViewBag.NewTypesID = new SelectList(GetNewsTypes(), "id", "title", selectedValue: null);
+            return View(news.ToList());
+        }
+
+        public List<NewsType> GetNewsTypes()
+        {
+            return db.NewsTypes.Where(nt => nt.hide == true).OrderBy(nt => nt.order).ToList();
+        }
+
+        // GET: Admin/News/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // GET: Admin/News/Create
+        public ActionResult Create()
+        {
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title");
+            return View();
+        }
+
+        // POST: Admin/News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,title,author,meta,hide,order,datebegin,brief_description,detail_description,newsTypeId,imagePath")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.News.Add(news);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title", news.newsTypeId);
+            return View(news);
+        }
+
+        // GET: Admin/News/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title", news.newsTypeId);
+            return View(news);
+        }
+
+        // POST: Admin/News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,title,author,meta,hide,order,datebegin,brief_description,detail_description,newsTypeId,imagePath")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(news).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.newsTypeId = new SelectList(db.NewsTypes, "id", "title", news.newsTypeId);
+            return View(news);
+        }
+
+        // GET: Admin/News/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: Admin/News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            News news = db.News.Find(id);
+            db.News.Remove(news);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Scripts/js/tinh-thanh.js
@@ -0,0 +1,67 @@
+﻿// ################ xử lý load địa chỉ
+let province_select = document.getElementById('province_id');
+let district_select = document.getElementById('district_id');
+let ward_select = document.getElementById('phuongxa');
+
+// api lấy tỉnh, thành phố
+fetch('https://vn-public-apis.fpo.vn/provinces/getAll?limit=-1')
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    let provinces = data.data.data;
+    // console.dir(provinces)
+    provinces.map(p => province_select.innerHTML += `<option value="${p.name}" code="${p.code}">${p.name_with_type}</option>`)
+})
+.catch(function(error) {
+    console.error("Xãy ra lỗi: ", error);
+});
+
+// api lấy quận huyện
+function loadDistric(province_code) {
+    fetch(`https://vn-public-apis.fpo.vn/districts/getByProvince?provinceCode=${province_code}&limit=-1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        let districts = data.data.data;
+        districts.map(d => district_select.innerHTML += `<option value="${d.name}" code="${d.code}">${d.name_with_type}</option>`)
+    });
+}
+
+// api lấy phường xã
+function loadWard(district_code) {
+    fetch(`https://vn-public-apis.fpo.vn/wards/getByDistrict?districtCode=${district_code}&limit=-1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        let wards = data.data.data;
+        wards.map(w => ward_select.innerHTML += `<option value="${w.name}" code="${w.code}">${w.name_with_type}</option>`)
+    });
+}
+
+// sự kiện chọn tỉnh
+province_select.addEventListener('change', function(e) {
+    if (this.selectedIndex == 0) {
+        district_select.options.length = 1 // xóa danh sách value
+    }
+    else {
+        let selected_option = this.options[this.selectedIndex];
+        // console.log(selected_option.getAttribute('code'));
+        district_select.options.length = 1 // xóa danh sách value
+        loadDistric(selected_option.getAttribute('code'));
+    }
+});
+
+// sự kiện chọn quận
+district_select.addEventListener('change', function(e) {
+    if (this.selectedIndex == 0) {
+        ward_select.options.length = 1 // xóa danh sách value
+    }
+    else {
+        let selected_option = this.options[this.selectedIndex];
+        ward_select.options.length = 1 // xóa danh sách value
+        loadWard(selected_option.getAttribute('code'));
+    }
+});
